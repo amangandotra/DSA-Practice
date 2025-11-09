4077c0372383969b0d059a5777f74e1c30aebcdc
amangandotra <amangandotra5744@gmail.com>
2025-11-10 00:11:12 +0530

    pattern 3

---
+++ Basic/Patterns/pattern_3_Number_Pyramid.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "enter number of rows";
+    int n;
+    cin >> n;
+    for (int i = 0; i<n;i++){
+        for(int j=1;j<i+2;j++){
+            cout << j;
+        }
+        cout << endl;
+    }
+}
