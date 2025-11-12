a9f6d774c763188956abf13bd05d627e18b6d12f
amangandotra <amangandotra5744@gmail.com>
2025-11-13 00:47:33 +0530

    pattern 14

--- Basic/Patterns/pattern_14_Alphabet_Triangle.cpp
+++ Basic/Patterns/pattern_14_Alphabet_Triangle.cpp
@@ -1,3 +1,22 @@
 // Pattern 14 – Alphabet Triangle
 // Print letters from `A` increasing across rows.
+// A 
+// A B
+// A B C
+// A B C D
+// A B C D E
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Enter number of rows";
+    int n;
+    cin>> n;
+    for (int i=0;i<n;i++){
+        for (int j=0;j<i+1;j++){
+            cout << char('A'+j) << " ";
+        }
+        cout << endl;
+    }
+}
